c01e3132320d247c23a9d36114a535e387f14efb
Tyler Schmidt <tylerdesu@icloud.com>
2020-04-24 16:21:40 +0900

    changed file structure

--- bin/j.sh
+++ bin/j.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Takes Japanese text from STDIN, looks it up on Jim Breen's WWWJDIC (http://nihongo.monash.edu/cgi-bin/wwwjdic?1C) and stores it in "j勉強"
 read input
-curl -sL http://nihongo.monash.edu/cgi-bin/wwwjdic?1MUJ$input|pup 'label:nth-child(2) text{}' | tail -n 2 >> data/j勉強
+curl -sL http://nihongo.monash.edu/cgi-bin/wwwjdic?1MUJ$input|pup 'label:nth-child(2) text{}' | tail -n 2 >> ../data/j勉強
