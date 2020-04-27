e2d120ef4168d0cd8233636451b4b96dd2856083
Tyler Schmidt <tylerdesu@icloud.com>
2020-04-27 10:58:55 +0900

    updated folder structure

--- bin/j.sh
+++ bin/j.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #Takes Japanese text from STDIN, looks it up on Jim Breen's WWWJDIC (http://nihongo.monash.edu/cgi-bin/wwwjdic?1C) and stores it in "j勉強"
-read input
-curl -sL http://nihongo.monash.edu/cgi-bin/wwwjdic?1MUJ$input|pup 'label:nth-child(2) text{}' | tail -n 2 >> ../data/j勉強
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+curl -sL http://nihongo.monash.edu/cgi-bin/wwwjdic?1MUJ$1 | pup 'label:nth-child(2) text{}' | tail -n 2 >> ../data/j勉強
+echo "$1 saved to j勉強 file."
+cat ../data/j勉強
