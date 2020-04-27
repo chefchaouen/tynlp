e33215a8a4e5725e37d77415348d8ca167928eae
Tyler Schmidt <tylerdesu@icloud.com>
2020-04-27 16:13:40 +0900

    added go file

--- j.sh
+++ j.sh
@@ -2,6 +2,6 @@
 #Takes Japanese text from STDIN, looks it up on Jim Breen's WWWJDIC (http://nihongo.monash.edu/cgi-bin/wwwjdic?1C) and stores it in "j勉強"
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
-curl -sL http://nihongo.monash.edu/cgi-bin/wwwjdic?1MUJ$1 | pup 'label:nth-child(2) text{}' | tail -n 2 >> ../data/j勉強
+curl -sL http://nihongo.monash.edu/cgi-bin/wwwjdic?1MUJ$1 | pup 'label:nth-child(2) text{}' | tail -n 2 >> /data/j勉強
 echo "$1 saved to j勉強 file."
 cat ../data/j勉強
